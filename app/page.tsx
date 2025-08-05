e7d398044cf2bd7bd8f96e2d025005d5b52c7a7a
kyohei <kyohei103@outlook.jp>
2025-08-05 18:12:24 +0900

    Deploy

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ export default function Page() {
     <main className="p-5">
       <h1 className="text-2xl mb-1 font-medium">記録帳</h1>
       <h2 className="text-[1.35rem] text-neutral-600">もっと自分の記録を書き残そう</h2>
+      <h2 className="text-[1.45rem] text-neutral-600 pl-[0.15rem]">kirokun v0.1.0</h2>
+      <h2 className="text-[1.35rem] text-neutral-600">それから･･･</h2>
     </main>
   )
 }
